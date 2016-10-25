798c20f59c6741ce900a0c5cc709a0369e0654f1
George <george.mausshardt@mst.edu>
2016-10-24 22:38:14 -0500

    Working Tutorial

--- app/src/main/java/george/mausshardt/com/sambirthday/MainCalendar.java
+++ app/src/main/java/george/mausshardt/com/sambirthday/MainCalendar.java
@@ -1,5 +1,6 @@
 package george.mausshardt.com.sambirthday;
 
+import android.icu.util.Calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ExpandableListView;
@@ -7,6 +8,7 @@ import android.widget.ExpandableListView;
 import java.util.ArrayList;
 
 import george.mausshardt.com.sambirthday.dataStructures.CalendarGroupItem;
+import george.mausshardt.com.sambirthday.dataStructures.CalendarListItem;
 import george.mausshardt.com.sambirthday.dataStructures.CalendarListViewAdapter;
 
 public class MainCalendar extends AppCompatActivity {
@@ -27,7 +29,28 @@ public class MainCalendar extends AppCompatActivity {
     }
 
     private ArrayList<CalendarGroupItem> setStandardGroups() {
+        ArrayList<CalendarGroupItem> outerList = new ArrayList<>();
+        ArrayList<CalendarListItem> innerList = new ArrayList<>();
+        CalendarGroupItem item1 = new CalendarGroupItem();
+        item1.setName("Passed");
 
+        CalendarGroupItem item2 = new CalendarGroupItem();
+        item2.setName("Present");
 
+        CalendarListItem month = new CalendarListItem();
+        month.setName("January");
+
+        CalendarListItem month2 = new CalendarListItem();
+        month2.setName("February");
+        innerList.add(month);
+        innerList.add(month2);
+
+        item1.setItems(innerList);
+        item2.setItems(innerList);
+
+        outerList.add(item1);
+        outerList.add(item2);
+
+        return outerList;
     }
 }
